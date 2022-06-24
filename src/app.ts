d8f06685dc691cfb787b5559abd1fb3cf14199b3
Faruq <CodingJay90>
2022-06-24 18:54:51 +0100

    delete on dropdown context

--- src/app.ts
+++ src/app.ts
@@ -25,38 +25,54 @@ import {
 import { selectDomElement } from "./utils";
 const req = new API();
 let DND = new DragNDrop();
-class GlobalState {
-  protected rootDirPath: string;
-  protected rootDirName: string;
-  protected workspaceName: string;
-  protected currentIdTarget: string | null;
-  protected isFolderSelected: boolean | null;
 
-  public constructor() {
-    this.rootDirPath = "";
-    this.rootDirName = "";
-    this.currentIdTarget = null;
-    this.isFolderSelected = null;
-    this.workspaceName = "Work space";
-  }
+interface StateInterface {
+  rootDirPath: string;
+  rootDirName: string;
+  workspaceName: string;
+  currentIdTarget: string | null;
+  isFolderSelected: boolean | null;
 }
 
-class File extends GlobalState {
+const state: StateInterface = {
+  rootDirPath: "",
+  rootDirName: "",
+  currentIdTarget: null,
+  isFolderSelected: null,
+  workspaceName: "Work space",
+};
+// class statee {
+//   protected rootDirPath: string;
+//   protected rootDirName: string;
+//   protected workspaceName: string;
+//   protected currentIdTarget: string | null;
+//   protected isFolderSelected: boolean | null;
+
+//   public constructor() {
+//     state.rootDirPath = "";
+//     state.rootDirName = "";
+//     state.currentIdTarget = null;
+//     state.isFolderSelected = null;
+//     state.workspaceName = "Work space";
+//   }
+// }
+
+class File {
   constructor() {
-    super();
     this.checkForFilesInDirectories =
       this.checkForFilesInDirectories.bind(this);
     this.handleFileClick = this.handleFileClick.bind(this);
   }
+
   private handleFileClick(e: MouseEvent): void {
     e.stopPropagation();
     const currentTarget = e.currentTarget as HTMLElement;
-    // console.log(this.workspaceName);
-
+    state.isFolderSelected = currentTarget.dataset.type === "folder";
+    state.currentIdTarget = currentTarget.dataset.file_id!;
+    console.log(state.currentIdTarget);
     let folder = new Folder();
     if (e.button === 2) {
-      this.currentIdTarget = currentTarget.dataset.file_id!;
-      this.isFolderSelected = currentTarget.dataset.type === "folder"; //set the "isFolderSelected" property on the Folder class to be used in the "deleteFolderOrFile method"
+      state.isFolderSelected = false; //currentTarget.dataset.type === "folder"; //set the "isFolderSelected" property on the Folder class to be used in the "deleteFolderOrFile method"
       folder.showDropDownContext(currentTarget.dataset.file_id!);
     }
   }
@@ -92,13 +108,11 @@ class File extends GlobalState {
       this.addEventListenerToFiles();
       Store.commit("setFiles", { id: i.file_id, file: i });
     });
-    // collapseAllFolders();
   }
 }
 
-class Folder extends GlobalState {
+class Folder {
   public constructor() {
-    super();
     let methods = Object.getOwnPropertyNames(Object.getPrototypeOf(this));
     methods
       .filter((method) => method !== "constructor")
@@ -110,9 +124,10 @@ class Folder extends GlobalState {
           _this[method] = _this[method].bind(this);
         }
       });
+
     this.onFolderClick = this.onFolderClick.bind(this);
     this.addFileToDirectory = this.addFileToDirectory.bind(this);
-    // this.deleteFileOrFolder = this.deleteFileOrFolder.bind(this);
+    this.deleteFileOrFolder = this.deleteFileOrFolder.bind(this);
   }
 
   private async onTextFieldChange(e: KeyboardEvent): Promise<void> {
@@ -123,18 +138,18 @@ class Folder extends GlobalState {
     try {
       const value = (e.target as HTMLInputElement).value;
       const fileIcon = selectDomElement(
-        `[id='${this.currentIdTarget}'] .text__field-icon`
+        `[id='${state.currentIdTarget}'] .text__field-icon`
       ) as HTMLElement;
       const fileName = value.includes(".") ? value.split(".")[0] : value; //exclude the extension name from filename
       const extName = value.includes(".")
         ? value.split(".")[value.split(".").length - 1]
         : ""; //check for extension name
       const selectedFolder =
-        Store.getState.folders[this.currentIdTarget!]?.path.split("\\") ||
-        this.rootDirPath.split("\\"); //add to root dir if no folder is selected
-      const index = selectedFolder.indexOf(this.rootDirName);
+        Store.getState.folders[state.currentIdTarget!]?.path.split("\\") ||
+        state.rootDirPath.split("\\"); //add to root dir if no folder is selected
+      const index = selectedFolder.indexOf(state.rootDirName);
       const newFilePath =
-        selectedFolder.slice(index + 1).join("/") || this.rootDirPath; // get the rest of the values in the array after a specific index
+        selectedFolder.slice(index + 1).join("/") || state.rootDirPath; // get the rest of the values in the array after a specific index
       const fileExt = value.split(".").pop();
       unmountComponent("textFieldErrorBox");
       fileIcon.innerHTML = renderIcon(`.${extName}`);
@@ -147,17 +162,17 @@ class Folder extends GlobalState {
             })
           );
 
-        if (this.isFolderSelected) {
+        if (state.isFolderSelected) {
           let newDirPath = `${newFilePath}\\${fileName.trim()}`;
           const res = await req.createDirectory(newDirPath);
           let folderId = uid();
           let folderSplit = newDirPath.split("\\");
           let folderName = folderSplit[folderSplit.length - 1];
-          let index = folderSplit.indexOf(this.rootDirName);
+          let index = folderSplit.indexOf(state.rootDirName);
 
           if (index === -1)
             newDirPath = `${
-              this.rootDirPath
+              state.rootDirPath
             }\\${newFilePath}\\${fileName.trim()}`;
 
           if (!res.data.success) {
@@ -182,11 +197,11 @@ class Folder extends GlobalState {
             FolderBlock({
               folder_name: fileName,
               id: folderId,
-              ...(this.currentIdTarget !== "folder-container" && {
+              ...(state.currentIdTarget !== "folder-container" && {
                 nested: "nested",
               }), //don't add the nested class if folder is added to the root directory
             }),
-            this.currentIdTarget!
+            state.currentIdTarget!
           );
         } else {
           let fileId = uid();
@@ -207,8 +222,8 @@ class Folder extends GlobalState {
               file_name: fileName,
               file_type: extName,
               file_dir: newFilePath
-                ? `${this.rootDirName}\\${newFilePath}\\${fileName}.${extName}`
-                : `${this.rootDirName}\\${fileName}.${extName}`,
+                ? `${state.rootDirName}\\${newFilePath}\\${fileName}.${extName}`
+                : `${state.rootDirName}\\${fileName}.${extName}`,
             },
           });
           renderComponent(
@@ -218,7 +233,7 @@ class Folder extends GlobalState {
               file_id: fileId,
               ext: `.${extName}`,
             }),
-            this.currentIdTarget!
+            state.currentIdTarget!
           );
           file.addEventListenerToFiles();
         }
@@ -244,8 +259,8 @@ class Folder extends GlobalState {
     e.stopPropagation();
     let currentTarget = e.currentTarget as HTMLElement;
     let folderId = currentTarget.dataset.folder_id as string;
-    this.currentIdTarget = folderId;
-    this.isFolderSelected = currentTarget.dataset.type === "folder";
+    state.currentIdTarget = folderId;
+    state.isFolderSelected = currentTarget.dataset.type === "folder";
 
     if (e.button === 0) {
       //detect a left click on folder-click
@@ -268,16 +283,16 @@ class Folder extends GlobalState {
 
   private addFileOrFolder(type: string): void {
     // fileOrFolder = type;
-    if (!this.currentIdTarget) {
-      this.currentIdTarget = "folder-container"; //update ui to add file or folder to root folder container
+    if (!state.currentIdTarget) {
+      state.currentIdTarget = "folder-container"; //update ui to add file or folder to root folder container
     }
-    const id = Store.getState.folders[this.currentIdTarget]
-      ? this.currentIdTarget
-      : (this.currentIdTarget = "folder-container"); //incase a folder is removed
+    const id = Store.getState.folders[state.currentIdTarget]
+      ? state.currentIdTarget
+      : (state.currentIdTarget = "folder-container"); //incase a folder is removed
 
     const parentFolder = selectDomElement(`[id='${id}']`) as HTMLElement;
     const isFileInput = type === "file";
-    this.isFolderSelected = !isFileInput;
+    state.isFolderSelected = !isFileInput;
     if (selectDomElement("#explorer__content-input"))
       unmountComponent("explorer__content-input");
     parentFolder.insertAdjacentHTML("beforeend", TextField({ isFileInput }));
@@ -298,7 +313,7 @@ class Folder extends GlobalState {
       ".explorer__content-headerNav ul"
     ) as HTMLUListElement;
 
-    workspaceNameContainer.textContent = `${this.workspaceName.substring(
+    workspaceNameContainer.textContent = `${state.workspaceName.substring(
       0,
       5
     )}...`;
@@ -310,22 +325,22 @@ class Folder extends GlobalState {
     e.stopPropagation();
 
     try {
-      if (this.isFolderSelected) {
-        let path = Store.getState.folders[this.currentIdTarget!].path;
+      if (state.isFolderSelected) {
+        let path = Store.getState.folders[state.currentIdTarget!].path;
         await req.deleteDirectory(path);
-        Store.commit("deleteFolder", this.currentIdTarget);
-        // delete Store.getState.folders[this.currentIdTarget!];
+        Store.commit("deleteFolder", state.currentIdTarget);
       } else {
-        let path = Store.getState.files[this.currentIdTarget!].file_dir;
-
-        // await req.deleteFile(path);
-        // delete this.File.files[this.currentIdTarget!];
-        // await http.delete("/files/delete", { data: { file_dir: path } });
+        if (state.currentIdTarget) {
+          let path = Store.getState.files[state.currentIdTarget].file_dir;
+          await req.deleteFile(path);
+          Store.commit("deleteFile", state.currentIdTarget);
+          req.deleteFile(path);
+        }
       }
-      unmountComponent(this.currentIdTarget!);
+      unmountComponent(state.currentIdTarget!);
     } catch (error) {
       console.log(error);
-      alert("An error occurred");
+      // alert("An error occurred");
     }
   }
 
@@ -369,6 +384,7 @@ class Folder extends GlobalState {
   protected addGlobalEventListener() {
     const addFileBtn = selectDomElement("#add__file") as HTMLElement;
     const addFolderBtn = selectDomElement("#add__folder") as HTMLElement;
+    let trashZone = selectDomElement("#trash__zone");
     const collapseFoldersBtn = selectDomElement(
       "#collapse__folders"
     ) as HTMLElement;
@@ -378,7 +394,6 @@ class Folder extends GlobalState {
     const explorerContainer = selectDomElement(
       ".explorer__content"
     ) as HTMLElement;
-    let trashZone = selectDomElement("#trash__zone");
 
     trashZone?.addEventListener("dragover", DND.trashZoneDragOver);
     trashZone?.addEventListener("drop", DND.dropInTrash);
@@ -420,7 +435,7 @@ class Folder extends GlobalState {
    */
   private async addFileToDirectory(directory: IFolder) {
     let selectedFolder = directory.path.split("\\");
-    let pathIndex = selectedFolder.indexOf(this.rootDirName);
+    let pathIndex = selectedFolder.indexOf(state.rootDirName);
     let newFilePath = selectedFolder.slice(pathIndex + 1).join("/");
     const res = await req.getFileInDirectory(newFilePath);
 
@@ -464,8 +479,8 @@ class Folder extends GlobalState {
       let {
         data: { directories, root_dir },
       } = await req.getDirectories();
-      this.rootDirPath = root_dir;
-      this.rootDirName = root_dir.split("\\").slice(-1)[0]; //.pop();
+      state.rootDirPath = root_dir;
+      state.rootDirName = root_dir.split("\\").slice(-1)[0]; //.pop();
 
       //wait for the foreach to run before moving to next function call
       let wait: Promise<void> = new Promise((resolve) => {
